f4be1c478801d1b250cf2cbd06937dd350657c29
Alexander Reimer <alexander.reimer2357@gmail.com>
2024-03-09 17:36:33 +0100

    added more tests for element deletion in DynArray

--- src/tests/java/TestDynArray.java
+++ src/tests/java/TestDynArray.java
@@ -56,11 +56,34 @@ public class TestDynArray {
 
     @Test
     @Order(5)
-    public void testDelete() {
+    public void testDeleteFirst() {
         dynArray.append(256);
         dynArray.append(512);
         dynArray.delete(0);
         assertEquals(1, dynArray.getLength(), "DynArray length should be 1 after deletion");
         assertEquals(512, dynArray.getItem(0), "Element at index 0 should be the second element after deletion");
+    }
+
+    @Test
+    @Order(6)
+    public void testDeleteLast() {
+        dynArray.append(256);
+        dynArray.append(512);
+        dynArray.append(1024);
+        dynArray.delete(3 - 1); // delete last element
+        assertEquals(2, dynArray.getLength(), "DynArray length should be 2 after deletion");
+        assertEquals(512, dynArray.getItem(1), "Element at index 1 should still be 512");
+    }
+
+    @Test
+    @Order(7)
+    public void testDeleteMiddle() {
+        dynArray.append(256);
+        dynArray.append(512);
+        dynArray.append(1024);
+        dynArray.delete(1); // delete last element
+        assertEquals(2, dynArray.getLength(), "DynArray length should be 2 after deletion");
+        assertEquals(256, dynArray.getItem(0), "Element at index 0 should still be 256");
+        assertEquals(1024, dynArray.getItem(1), "Element at index 1 should now be 1024");
     }
 }
