da5e2c1046b7b1bc85d97634bd539439b6145b0e
GelbEinhalb <exzitonyw@gmail.com>
2024-05-22 11:09:18 +0200

    Remove test commit (#7)

--- src/main/java/BinTree.java
+++ src/main/java/BinTree.java
@@ -7,8 +7,6 @@ package main.java;
  * @author Yannick Weigert (GitHub: GelbEinhalb, email: yannick@gelbeinhalb.com)
  * @version 09.03.2024
  *
- *
- * Test commit for BlueJ Package
  */
 public class BinTree<Type> {
 
